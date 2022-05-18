0cba7e82a1e2b4ec046577dee69cd8ac5215b988
17504408918 <1043097600@qq.com>
2022-05-19 00:42:51 +0800

    付款页面加上啦

---
+++ src/main/java/com/example/demo1/PaymentController.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PaymentController implements Initializable {
+    public static String user_id;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}
