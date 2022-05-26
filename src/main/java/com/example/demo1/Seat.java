600a089e023e532eafbaa5b3f342512c201cbbfc
MWDZ <jinjunfeng@bupt.edu.cn>
2022-05-26 11:44:04 +0800

    add go back

--- src/main/java/com/example/demo1/Seat.java
+++ src/main/java/com/example/demo1/Seat.java
@@ -102,6 +102,33 @@ public class Seat {
         seat[row_num][column_num] = 1;
         update(seat);
     }
+
+
+    /**
+     * Select the available seat
+     */
+    public void update(String selectedSeat, int flag) {
+        int len = selectedSeat.length();
+        String row;
+        char column;
+        column = selectedSeat.charAt(0);
+        if (len == 2) {
+            row = ""+selectedSeat.charAt(1);
+
+        } else {
+            row = String.valueOf(selectedSeat.charAt(1)) + String.valueOf(selectedSeat.charAt(2));
+        }
+        int row_num = Integer.parseInt(row)-1;
+        int column_num=0;
+
+        column_num = parseSeat(column);
+
+
+        int[][] seat = this.findSeatState();
+        seat[row_num][column_num] = 0;
+        update(seat);
+    }
+
     /**
      * Find all the remaining seats
      * @return Arraylist of remaining seats
