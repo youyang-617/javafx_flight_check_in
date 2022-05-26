092ae85da57dee24c80cd78aa0ce9d3c2f8888c3
17504408918 <1043097600@qq.com>
2022-05-26 11:15:40 +0800

    pay页面完结撒花

--- src/main/java/com/example/demo1/PaymentController.java
+++ src/main/java/com/example/demo1/PaymentController.java
@@ -37,6 +37,7 @@ public class PaymentController implements Initializable {
     @FXML
     private Button next_pay;
 
+    private Boolean flag=false;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -51,14 +52,27 @@ public class PaymentController implements Initializable {
 
     @FXML
     private void GoBack() throws IOException {
-        PageController controller = new PageController();
-        controller.change_page(goback_pay);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Warning");
+        alert.setHeaderText("You need to pay again for changed food or seat, operation irreversible!");
+        alert.setContentText("The original payment will be returned to your card within 3 working days");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            PageController controller = new PageController();
+            controller.change_page(goback_pay);
+        }
 
     }
     @FXML
     private void Next() throws IOException {
-        PageController controller = new PageController();
-        controller.change_page(next_pay);
+        if(flag){
+            PageController controller = new PageController();
+            controller.change_page(next_pay);
+        }else{
+            num_label.setText("You haven't finish your payment!");
+        }
+
 
     }
     @FXML
@@ -69,6 +83,7 @@ public class PaymentController implements Initializable {
         if (InputCheck.checkCreditCardNumber(Card)&&InputCheck.checkCreditCardPassword(Pswd)) {
 
             if(CreditCards.checkCreditCards(Card, Pswd)){
+                flag = true;
                 num_label.setText("");
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Confirmation");
